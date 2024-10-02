170f08a75eb7f265ceba6257e0d64ee774dbbe2f
Américo <americochiclana@gmail.com>
2024-10-02 12:29:48 +0200

    creating the project

---
+++ website2.0/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { gsap } from 'gsap';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
